f6f9d7d20897050bbfd6b0920e6e75cde1132df9
Yaroslav KS <jarik.kuchenko@gmail.com>
2023-06-02 16:03:13 +0300

    add Admin Basic design

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout"
+
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-     <div className="bg-blue-900 w-screen h-screen flex items-center">
-       <div className="text-center w-full">
-         <button onClick={()=> signIn("google")} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-       </div>
-     </div>
-    );
-  }
-  return (
-    <div className="bg-blue-900 min-h-screen">
-      
-      <div>Your logged in! {session.user.email}</div>
+  const {data: session} = useSession()
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Welcome, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-10 h-10"/>
+        <span className="py-1  px-2">
+        {session?.user?.name}
+        </span>
+      </div>
     </div>
-  );
+  </Layout>
 }
