33ee69ba5ca625a0879014ef600e060f96f8d32b
Yaroslav KS <jarik.kuchenko@gmail.com>
2023-06-09 16:20:03 +0300

    admin setup with tailwind  admin authentication admin basic design  admin - products management  admin - categories management  admin - security making admin mobile responsive

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Layout from "@/components/Layout"
-
+import Layout from "@/components/Layout";
+import {useSession} from "next-auth/react";
 
 export default function Home() {
-  const {data: session} = useSession()
+  const {data: session} = useSession();
   return <Layout>
     <div className="text-blue-900 flex justify-between">
       <h2>
-        Welcome, <b>{session?.user?.name}</b>
+        Hello, <b>{session?.user?.name}</b>
       </h2>
       <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
-        <img src={session?.user?.image} alt="" className="w-10 h-10"/>
-        <span className="py-1  px-2">
-        {session?.user?.name}
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
         </span>
       </div>
     </div>
